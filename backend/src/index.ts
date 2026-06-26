897c17b5b0d6a29ebd1b9994e359ed56a95f6c3f
Kartik Sharma <poojan4604@gmail.com>
2026-06-26 23:19:47 +0530

    feat: add create and join room in websockets
    - Implement CREATE_ROOM event
    - Implement JOIN_ROOM event
    - Add room validation and error handling
    - Send ROOM_CREATED and ROOM_JOINED events
    - Track connected users and active rooms
    - Add frontend UI for creating and joining rooms

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,17 +23,25 @@ new Elysia()
       id,ws
     })
     ws.send(JSON.stringify({
-      type: 'connected',
+      type: EVENTS.CONNECTED,
       userId: id
     }))
 
     console.log(id)
   }
   ,
- message(ws,rawdata:string){
-  const data=JSON.parse(rawdata);
-
+ message(ws,rawdata:any){
+  console.log("this is message handler")
+  try {
+    console.log(rawdata);
+    
+    const data = rawdata as {
+      type: string;
+      roomId?:string
+    };
+  
   switch(data.type){
+
     case EVENTS.CONNECTED:
       break
     case EVENTS.CREATE_ROOM:
@@ -43,14 +51,65 @@ new Elysia()
       room.gameStatus='waiting';
       rooms.set(roomId,room);
       console.log(room)
+      console.log(roomId)
+      
       ws.send(JSON.stringify({
-        type: EVENTS.ROOM_CREATED,
+        type: 'ROOM_CREATED',
         roomId
       }))
       break
     case EVENTS.JOIN_ROOM:
+      const roomIdtoJoin=data.roomId;
+      const joinedID=(ws.data as any).userId;
+      if(!roomIdtoJoin){
+        console.error("roomId is not available")
+        ws.send(JSON.stringify({ type: "ERROR", message: "No room ID provided" }));
+        break;
       
+      }
+     const targetRoom= rooms.get(roomIdtoJoin);
+     if(!targetRoom){
+      console.error("Room doesnt exist")
+      ws.send(JSON.stringify({ type: "ERROR", message: "No room exists" }));
+      break;
+     }
 
+     if(targetRoom.gameStatus!=='waiting'){
+      ws.send(JSON.stringify({
+        type:"ERROR",
+        message:"The game is already active"
+      }))
+
+     }
+     if(targetRoom.isFull()){
+      ws.send(JSON.stringify({
+        type:"ERROR",
+        message:"The room is full"
+      }))
+
+      return;
+     }
+     targetRoom.addPlayer(joinedID);
+     targetRoom.gameStatus='active';
+
+     const joinPayload = JSON.stringify({
+      type: 'ROOM_JOINED',
+      roomId: roomIdtoJoin,
+      players:targetRoom.players,
+      gameStatus: targetRoom.gameStatus,
+      // You can pass specific player assignments here if your room tracks them:
+      // whitePlayer: targetRoom.creatorId,
+      // blackPlayer: joinerId
+    });
+    for (const playerId of targetRoom.players) {
+      const player = users.get(playerId);
+  
+      if (player) {
+          player.ws.send(joinPayload);
+      }
+  }
+
+  
       break
     case EVENTS.MOVE:
       break
@@ -66,9 +125,15 @@ new Elysia()
 
 
 
+  } catch (error) {
+    console.error(error)
+  }
+   
 
   }
 
 })
 
-.listen(3500)
+.listen(3500,()=>{
+  console.log("server starts at 3500")
+})
