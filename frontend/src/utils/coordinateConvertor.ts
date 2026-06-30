c28825c60d553a73ced208ec1c1055ea3d3cbd75
Kartik Sharma <poojan4604@gmail.com>
2026-06-30 20:32:53 +0530

    CC-48 Remove obsolete frontend src after Next.js migration

--- frontend/src/utils/coordinateConvertor.ts
+++
@@ -1,9 +0,0 @@
-const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
-
-export function coordToSquare(coord: [number, number]) {
-
-    const [x, y] = coord;
-
-    return files[x] + (8 - y);
-
-}
