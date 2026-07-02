393c91424292bc8d73681f6d781a7a808302a2fc
Ali Ahmed <ali.ali.ameen245@gmail.com>
2026-07-02 21:31:12 +0300

    feat: add error for game finished

--- backend/src/server/protocol/errors.ts
+++ backend/src/server/protocol/errors.ts
@@ -6,6 +6,7 @@ export const NOT_AUTHENTICATED = "not-authenticated" as const;
 export const NOT_IN_GAME = "not-in-game" as const;
 export const ROOM_NOT_FOUND = "room-not-found" as const;
 export const GAME_FULL = "game-full" as const;
+export const GAME_FINISHED = "game-finished" as const;
 export const INTERNAL_ERROR = "internal-error" as const;
 
 export type ErrorCode =
@@ -15,4 +16,5 @@ export type ErrorCode =
   | typeof NOT_IN_GAME
   | typeof ROOM_NOT_FOUND
   | typeof GAME_FULL
+  | typeof GAME_FINISHED
   | typeof INTERNAL_ERROR;
