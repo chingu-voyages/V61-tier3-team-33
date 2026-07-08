b111612ca7a8a226b9980d1c08b0bd18274ce9c9
Ali Ahmed <ali.ali.ameen245@gmail.com>
2026-07-08 20:18:19 +0300

    feat: wired socket,  game, session, toast to app provider

--- frontend/context/app/AppProvider.tsx
+++ frontend/context/app/AppProvider.tsx
@@ -1,9 +1,11 @@
 "use client"
 
+import { GooeyToaster } from "@/components/ui/goey-toaster"
 import { ThemeProvider } from "@/context/theme/ThemeProvider"
 import { ChessThemeProvider } from "@/context/theme/ChessThemeProvider"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { SocketProvider } from "@/socket/provider"
+import { SessionProvider } from "@/context/session/SessionProvider"
 import { GameProvider } from "@/context/game/GameProvider"
 import { DebugPanel } from "@/components/debug/DebugPanel"
 import { env } from "@/config/env"
@@ -13,11 +15,14 @@ export function AppProvider({ children }: { children: React.ReactNode }) {
     <ThemeProvider>
       <ChessThemeProvider>
         <TooltipProvider>
+          <GooeyToaster position="top-right" closeButton showProgress />
           <SocketProvider url={env.socketUrl}>
-            <GameProvider>
-              {children}
-              <DebugPanel />
-            </GameProvider>
+            <SessionProvider>
+              <GameProvider>
+                {children}
+                <DebugPanel />
+              </GameProvider>
+            </SessionProvider>
           </SocketProvider>
         </TooltipProvider>
       </ChessThemeProvider>
